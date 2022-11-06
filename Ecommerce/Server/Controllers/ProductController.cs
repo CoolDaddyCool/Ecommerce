341056d9e4caf836bbe1dc30bb984539722472e1
franczakp <piotr.franczak@fujitsu.com>
2022-11-06 08:49:49 +0100

    Add project files.

---
+++ Ecommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext db;
+
+        public ProductController(DataContext db)
+        {
+            this.db = db;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await db.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
---
+++ Ecommerce/Shared/Product.cs
@@ -0,0 +1,16 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Shared;
+
+public  class Product
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+}
